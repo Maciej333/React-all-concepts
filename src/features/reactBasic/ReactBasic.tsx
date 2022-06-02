c821bb44c087793b6f2aaea4800c4db4b22e220e
Maciej333 <maciej.rogowski333@gmail.com>
2022-06-02 15:23:15 +0200

    add elements style, add stateAndProp component

--- src/features/reactBasic/ReactBasic.tsx
+++ src/features/reactBasic/ReactBasic.tsx
@@ -1,9 +1,19 @@
 import './ReactBasic.style.scss';
+import StateAndProp from './StateAndProp/StateAndProp';
 
 export default function ReactBasic() {
     return (
-        <div>
+        <div className='react-basic'>
             <h1>React Basic</h1>
+            <div className='minor'>
+                <h2>state and props</h2>
+                <StateAndProp propString='Value for prop string'>
+                    <p>Children Paragraph</p>
+                    <div>
+                        <p>div paragraph <span>ABC</span></p>
+                    </div>
+                </StateAndProp>
+            </div>
         </div>
     )
 }
---
+++ src/features/reactBasic/StateAndProp/StateAndProp.tsx
@@ -0,0 +1,62 @@
+import './StateAndProp.style.scss';
+import { ReactNode, useState } from "react";
+
+export default function StateAndProp(props: { children: ReactNode, propString: string, propNumber?: number }) {
+
+    const { children, propString, propNumber } = props;
+    const [stateString, setStateString] = useState("DANE");
+    const [stateNumber, setStateNumber] = useState(10);
+    const [stateObject, setStateObject] = useState({
+        name: "Alina",
+        age: 10
+    })
+
+    const handleClickString = (value: string) => {
+        setStateString(value);
+    }
+
+    const handleClickNumber = (value: number) => {
+        setStateNumber(value);
+    }
+
+    const handleClickObject = (value: { name: string, age: number }) => {
+        setStateObject(value);
+    }
+
+    return (
+        <div className="state-prop">
+            <div className='state'>
+                <span>State string = </span>
+                <span>{stateString}</span>
+                <button onClick={() => handleClickString("NOWE DANE")}>NEW</button>
+                <button onClick={() => handleClickString("DANE")}>INIT</button>
+
+                <span>State number = </span>
+                <span>{stateNumber}</span>
+                <button onClick={() => handleClickNumber(99)}>NEW</button>
+                <button onClick={() => handleClickNumber(10)}>INIT</button>
+
+                <span>State object = </span>
+                <div>
+                    <p>name = {stateObject.name}</p>
+                    <p>age = {stateObject.age}</p>
+                </div>
+                <button onClick={() => handleClickObject({ name: "Tadeusz", age: 40 })}>NEW</button>
+                <button onClick={() => handleClickObject({ name: "Alina", age: 10 })}>INIT</button>
+
+            </div>
+            <div className="props">
+                <span>Prop String =</span>
+                <span> {propString}</span>
+
+                <span>Prop Number =</span>
+                <span> {propNumber ? propNumber : "[UPS] value from parent not given"}</span>
+
+                <span>Prop children</span>
+                <div>
+                    {children}
+                </div>
+            </div>
+        </div >
+    )
+}
